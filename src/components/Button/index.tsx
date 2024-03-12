331323147d52b2801d914ba0d0798bba9f42da9b
Waydj <farhminait@gmail.com>
2024-03-12 12:32:05 +0300

    button stories

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,68 @@
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/utils";
 
-type ButtonProps = ComponentProps<"button">;
+const buttonStyles = cva(
+  [
+    "w-full",
+    "p-2",
+    "rounded",
+    "font-semibold",
+    "focus:outline-none",
+    "disabled:cursor-not-allowed",
+  ],
+  {
+    variants: {
+      variant: {
+        solid: "",
+        outline: "border-2",
+        ghost: "transition-colors duration-300",
+      },
+      size: {
+        small: "text-sm",
+        medium: "text-base",
+        large: "text-lg",
+      },
+      colorScheme: {
+        primary: "text-white",
+      },
+    },
+    compoundVariants: [
+      {
+        variant: "solid",
+        colorScheme: "primary",
+        className: "bg-primary-500 hover:bg-primary-600",
+      },
+      {
+        variant: "outline",
+        colorScheme: "primary",
+        className:
+          "text-primary border-primary-500 bg-transparent hover:bg-primary-100",
+      },
+      {
+        variant: "ghost",
+        colorScheme: "primary",
+        className: "text-primary-600 bg-transparent hover:bg-primary-100",
+      },
+    ],
+    defaultVariants: {
+      variant: "solid",
+      size: "medium",
+      colorScheme: "primary",
+    },
+  }
+);
 
-export const Button = ({ ...props }: ButtonProps) => {
-  return <button className="text-blue-500" {...props} />;
-};
+type ButtonProps = ComponentProps<"button"> & VariantProps<typeof buttonStyles>;
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ variant, size, colorScheme, className, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={cn(buttonStyles({ variant, size, colorScheme, className }))}
+        {...props}
+      />
+    );
+  }
+);
